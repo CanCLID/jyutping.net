b4563ac32dd12e33b3e47609c6d1a2f30899a2e4
graphemecluster <graphemecluster@gmail.com>
2021-08-13 22:56:45 +0800

    Add Tab

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,17 @@ const mapping = { [-1]: 3, 0: 0, 1: 1, 2: 2, 3: 4 };
 const target = [/Win/i, /Mac/i, /Linux/i, /iPhone|iPad|iPod/i].findIndex(item => item.test(navigator.platform));
 
 const platforms = document.getElementById("platforms");
-const display = platforms.children[mapping[target]].firstElementChild;
+const display = platforms.children[mapping[target]] as HTMLAnchorElement;
 
-const logo = display.firstElementChild;
-const name = display.children[1].textContent;
+const link = display.href;
+const inner = display.firstElementChild;
+
+const logo = inner.firstElementChild;
+const name = inner.children[1].textContent;
 
 ["btn-download-header", "btn-download-hero", "btn-download-final"].forEach(id => {
-  const element = document.getElementById(id);
+  const element = document.getElementById(id) as HTMLAnchorElement;
   element.appendChild(logo.cloneNode(true));
   element.appendChild(document.createTextNode(`下載 ${name} 版`));
+  element.href = link;
 });
