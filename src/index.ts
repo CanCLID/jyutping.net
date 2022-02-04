e18d76411c64c95e63d2a2c0ad95872f9d5ea0e3
graphemecluster <graphemecluster@gmail.com>
2022-02-05 01:13:14 +0800

    Use strict mode, Add ESLint, Adjust Format

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,53 @@
+import "core-js/actual/array/find-last-index";
 import "./index.css";
 
 // Correct height for mobiles
-const hero = document.getElementById("hero");
+const hero = document.getElementById("hero")!;
 const resize = () => {
   hero.style.minHeight = innerHeight + "px";
 };
 addEventListener("resize", resize);
 addEventListener("load", resize);
 
-// Set download buttons
-// 0: Windows
-// 1: iOS
-// 2: Android
-// 3: Linux
-// 4: Mac
-const mapping = { [-1]: 0, 0: 0, 1: 4, 2: 3, 3: 2, 4: 1 };
-const target = mapping[[/Win/i, /iPhone|iPad|iPod/i, /Android/i, /Linux/i, /Mac/i].findIndex(r => r.test(navigator.userAgent))];
+/*
+Set download buttons
+0: Windows (fallback)
+1: Mac
+2: Linux
+3: Android
+4: iOS
+*/
+const target = ["", "Mac", "Linux", "Android", "iPhone|iPad|iPod"].findLastIndex(r => new RegExp(r, "i").test(navigator.userAgent));
 
-const platforms = document.getElementById("platforms");
+const platforms = document.getElementById("platforms")!;
 const display = platforms.children[target] as HTMLAnchorElement;
 
 const link = display.href;
-const inner = display.firstElementChild;
+const inner = display.firstElementChild!;
 
-const logo = inner.firstElementChild;
+const logo = inner.firstElementChild!;
 const name = inner.children[1].textContent;
 
 ["btn-download-hero", "btn-download-final"].forEach(id => {
   const element = document.getElementById(id) as HTMLAnchorElement;
   element.appendChild(logo.cloneNode(true));
-  if (target !== 2) {
-    element.appendChild(document.createTextNode(`下載 ${name} 版`));
-  } else {
-    element.appendChild(document.createTextNode("睇下 Linux 下點搞"));
-  }
+  element.appendChild(document.createTextNode(target === 2 ? "睇下 Linux 下點搞" : `下載 ${name} 版`));
   element.href = link;
 });
 
 // Make "其他版本" button scroll smoothly
-const other = document.getElementById("other");
+const other = document.getElementById("other")!;
 
-document.getElementById("btn-more").addEventListener("click", event => {
+document.getElementById("btn-more")!.addEventListener("click", event => {
   event.preventDefault();
   other.scrollIntoView({ behavior: "smooth", block: "start" });
   return false;
 });
 
 // Make "我已經用緊 Rime" button scroll smoothly
-const faq = document.getElementById("faq");
+const faq = document.getElementById("faq")!;
 
-document.getElementById("btn-rime").addEventListener("click", event => {
+document.getElementById("btn-rime")!.addEventListener("click", event => {
   event.preventDefault();
   faq.scrollIntoView({ behavior: "smooth", block: "start" });
   return false;
@@ -57,7 +55,7 @@ document.getElementById("btn-rime").addEventListener("click", event => {
 
 // Add Windows logo for Windows keyboard key
 [].forEach.call(document.getElementsByClassName("windows-key"), (item: HTMLElement) => {
-  item.appendChild(platforms.firstElementChild.firstElementChild.firstElementChild.cloneNode(true));
+  item.appendChild(platforms.firstElementChild!.firstElementChild!.firstElementChild!.cloneNode(true));
 });
 
 // Set default tabs according to target platform
@@ -92,20 +90,3 @@ new IntersectionObserver(
   },
   { root: null, rootMargin: "0px", threshold: 0.25 }
 ).observe(other);
-
-/*
-// auto scroll through hero
-const hero = document.getElementById("hero");
-const secondary = document.getElementById("secondary");
-
-let prev = 0.98;
-new IntersectionObserver(
-  entries => {
-    entries.forEach(({ intersectionRatio }) => {
-      (intersectionRatio > prev ? hero : secondary).scrollIntoView({ behavior: "smooth", block: "start" });
-      prev = intersectionRatio;
-    });
-  },
-  { root: null, rootMargin: "0px", threshold: [0.01, 0.99] }
-).observe(hero);
-*/
